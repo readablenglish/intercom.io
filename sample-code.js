f9ad76229b35bad8a30fbfc4805085accf3a9048
zeeshanqk <zeeshan@readhowyouwant.com>
2015-07-10 13:21:13 +1000

    new file
    snippet of the code of a sample integration.

---
+++ sample-code.js
@@ -0,0 +1,15 @@
+<script>
+  window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+	// TODO: The current logged in user's type [Individual, Parent, Teacher]. The key "Person Type" and the values are case sensitive.
+	"Person Type": "Individual"
+	//DO NOT MODIFY THE APP_ID
+    app_id: "gtzkmls4"
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/gtzkmls4';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
